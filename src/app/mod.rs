1afabe3a3deda3e01b5c57b8b924f82f39303ca2
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-04-10 01:20:33 +0200

    Update API + Add custom update function for user

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -9,39 +9,55 @@ use winit::{
     window::WindowBuilder,
 };
 
-pub struct App {}
+pub type UpdateFn<T> = dyn FnMut(&mut T, &mut Vec<String>);
 
-impl App {
-    /// Creates a new **Renderer** instance
-    pub fn new() -> Self {
-        return App {};
+/// The root component of any smoothie application
+pub struct App<T> {
+    /// Custom user state that is provided in update
+    user_state: T,
+    /// Custom user update function
+    update_fn: Box<UpdateFn<T>>,
+    /// Represents the "dom" the user can interact with
+    dom: Vec<String>,
+}
+
+impl<T> App<T> {
+    /// Creates a new **App** instance with provided user state
+    pub fn new(user_state: T, update_fn: Box<UpdateFn<T>>) -> Self {
+        let dom = Vec::new();
+
+        Self {
+            user_state,
+            update_fn,
+            dom,
+        }
     }
 
     /// Runs the current **application** built
-    pub async fn run(&self) {
+    pub async fn run(&mut self) {
         env_logger::init();
 
         // To make the event loop closure work, either use a mut event_loop and event_loop.run_return() or pass self by value
         let mut event_loop = EventLoop::new();
         let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-        // Initialize rendering state
-        let mut state = renderer::State::new(&window).await;
+        // Initialize renderer
+        let mut renderer = Renderer::new(&window).await;
 
         event_loop.run_return(move |event, _, control_flow| match event {
             Event::WindowEvent {
                 ref event,
                 window_id,
             } if window_id == window.id() => {
-                if !state.input(event) {
+                if !renderer.input(event) {
                     // Updated event handle!
                     match event {
                         WindowEvent::Resized(physical_size) => {
-                            state.resize(*physical_size);
+                            renderer.resize(*physical_size);
                         }
                         WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
                             // new_inner_size is &&mut so we have to dereference it twice
-                            state.resize(**new_inner_size);
+                            renderer.resize(**new_inner_size);
                         }
                         WindowEvent::CloseRequested
                         | WindowEvent::KeyboardInput {
@@ -58,12 +74,13 @@ impl App {
                 }
             }
             Event::RedrawRequested(window_id) if window_id == window.id() => {
-                state.update();
+                // Update sketch by calling the user's update function
+                (self.update_fn)(&mut self.user_state, &mut self.dom);
 
-                match state.render() {
+                match renderer.render(&self.dom) {
                     Ok(_) => {}
                     // Reconfigure the surface if lost
-                    Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                    Err(wgpu::SurfaceError::Lost) => renderer.resize(renderer.size),
                     // The system is out of memory, we should probably quit
                     Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
                     // All other errors (Outdated, Timeout) should be resolved by the next frame
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,33 @@
 fn main() {
-    let app = smoothie::App::new();
-    pollster::block_on(app.run());
+    UserRenderer::new().run();
+}
+
+struct CustomRenderState {
+    count: i32,
+}
+
+struct UserRenderer {
+    state: CustomRenderState,
+}
+
+impl UserRenderer {
+    pub fn new() -> Self {
+        Self {
+            state: CustomRenderState { count: 0 },
+        }
+    }
+
+    // Pass self by value here
+    pub fn run(self) {
+        let mut app = smoothie::App::new(
+            self.state,
+            Box::new(|state: &mut CustomRenderState, dom: &mut Vec<String>| {
+                //println!("Count: {:?}", state.count);
+                //println!("DOM: {:#?}", dom);
+                state.count += 1;
+            }),
+        );
+
+        pollster::block_on(app.run());
+    }
 }
