ddb8652096962c65c1dfaab276b835f3282b98ce
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-05-13 23:29:59 +0200

    Update example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use smoothie::Smoothie;
+use std::f32::consts::PI;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -10,16 +11,17 @@ fn update(smoothie: &mut Smoothie) {
     {
         let dom = smoothie.dom();
 
-        dom.insert(0, Box::new(smoothie::Arrow::new(0.8)));
+        dom.insert(0, Box::new(smoothie::Arrow::new(0.4)));
 
         smoothie.commit();
     }
 
     let mut duration = smoothie.time_since_start().as_secs_f32();
-    while duration < 10.0 {
+
+    loop {
         let dom = smoothie.dom();
 
-        dom.get_mut(&0).unwrap().set_scale(duration / 10f32);
+        dom.get_mut(&0).unwrap().set_scale((duration % 2f32) / 2f32);
 
         smoothie.commit();
         duration = smoothie.time_since_start().as_secs_f32();
